03f981a281b87b318c3bd9e3760f7204a616fa77
John Martinez <jmartinez729@gmail.com>
2018-08-26 20:44:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,4 +46,17 @@ class TicTacToe
     turn_count % 2 == 0 ? "X" : "O"
   end
   
+  def turn
+    puts "Please choose a number 1-9:"
+    user_input = gets.chomp
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      player_token = current_player(board)
+      move(index, player_token)
+      display_board
+    else
+      turn
+    end
+  end
+  
 end 
