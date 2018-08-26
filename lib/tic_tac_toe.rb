69205d3fc185019e19602623b40bc91e14a4bde2
John Martinez <jmartinez729@gmail.com>
2018-08-26 20:42:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,14 +38,14 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(index)
   end
   
-  def turn_count(board)
-  turn = 0
-  board.each do |index|
-    if index == "X" || index == "O"
-      turn += 1
+  def turn_count
+    turn = 0
+    @board.each do |index|
+      if index == "X" || index == "O"
+        turn += 1
+      end
     end
+    return turn
   end
-  return turn
-end
   
 end 
