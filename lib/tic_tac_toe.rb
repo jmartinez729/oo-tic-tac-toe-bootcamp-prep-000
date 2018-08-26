b5ce7e8c2c1f764ddd7e97509e2441559a681bb3
John Martinez <jmartinez729@gmail.com>
2018-08-26 20:47:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,4 +82,12 @@ class TicTacToe
     @board.all? {|index| index == "X" || index == "O"}
   end
   
+  def draw?
+    if !won? && full?
+      return true
+    else
+      return false
+    end
+end
+  
 end 
